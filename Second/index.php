5d591247793d994a536d7c47cc93e70f234e4ed6
arju88nair <arju88nair@gmail.com>
2019-01-03 21:54:31 +0530

    Finished calculator

--- Second/index.php
+++ Second/index.php
@@ -1,57 +1,74 @@
 <?php
 require_once 'utils/Calculator.php';
-$html="";
+$html = "";
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['estimated_car_value'], $_POST['tax_percentage'], $_POST['instalments'])) {
-    $estimated_car_value = (int) $_POST['estimated_car_value'];
-    $tax_percentage = (int) $_POST['tax_percentage'];
-    $instalments = (int) $_POST['instalments'];
-    $calculate = new Calculate($estimated_car_value,$tax_percentage,$instalments);
+    $estimated_car_value = (int)$_POST['estimated_car_value'];
+    $tax_percentage = (int)$_POST['tax_percentage'];
+    $instalments = (int)$_POST['instalments'];
+    $calculate = new Calculate();
+    $calculate = $calculate->calculatePremium($estimated_car_value, $tax_percentage, $instalments);
+    $html = $calculate;
 }
-
 ?>
-
 
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="css.css">
     <title>Simple Car InsuranceCalculator</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <style>
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+
+        th, td {
+            padding: 15px;
+        }
+    </style>
 </head>
+
 <body>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <a class="navbar-brand" href="#">Simple Insurance Calculator</a>
+</nav>
 <br>
 <div class="container">
-<div class="row align-items-center h-100">
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-        Estimated value of the car (100 - 100 000 EUR): <input type="number" name="estimated_car_value" value=""
-                                                               required>
-        <br><br>
-
-        Tax percentage (0 - 100%): <input type="number" name="tax_percentage" value="">
-        <br><br>
-
-        Number of instalments(1-12): <select name="instalments">
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">Audi</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-            <option value="11">11</option>
-            <option value="12">12</option>
-        </select>
-        <br><br>
-        <input type="submit" name="submit" value="Calculate">
-    </form>
+    <div class="row align-items-center h-100">
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+            Estimated value of the car (100 - 100 000 EUR): <input type="number" name="estimated_car_value" value=""
+                                                                   required>
+            <br><br>
+            Tax percentage (0 - 100%): <input type="number" name="tax_percentage" value="">
+            <br><br>
+            Number of instalments(1-12): <select name="instalments">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+                <option value="11">11</option>
+                <option value="12">12</option>
+            </select>
+            <br><br>
+            <input type="submit" name="submit" value="Calculate">
+        </form>
+    </div>
 </div>
-  </div>
+<br>
+<div>
+    <?php
+    echo $html;
+    ?>
 
+</div>
 
 </body>
 </html>
--- Second/utils/Calculator.php
+++ Second/utils/Calculator.php
@@ -1,61 +1,121 @@
 <?php
 /**
  * Written by Nair, 1/1/19 8:59 PM
+ * Decided to use PHP 7.*'s strict typing for this particular task
  */
 
 class Calculate
 {
 
-    private $commissionRate = 0.17;
-
+    private $commissionRate = 17;
 
     /**
-     * Calculate constructor.
-     * @param int $estimated_car_value
-     * @param int $instalments
-     * @param int $tax_percentage
-     * * Calculate constructor.
      * Doing Date time conditions
      */
-
-
-    public function __construct(int $estimated_car_value, int $instalments, int $tax_percentage)
+    public function __construct()
     {
         $time = date("H");
         $day = date("D");
+        $this->rate = 11;
 
         if ($day == "Fri" && $time >= 15 && $time <= 20) {
-            $premiumRate = 13;
-            $basePremium = $estimated_car_value * ($premiumRate / 100);
-        } else {
-            $premiumRate = 11;
-            $basePremium = $estimated_car_value * ($premiumRate / 100);
+            $this->rate = 13;
         }
+    }
 
+
+    /**
+     * @param $estimated_car_value
+     * @param $tax_percentage
+     * @param $instalments
+     * @return string
+     * For calculating the required fields
+     */
+    public function calculatePremium($estimated_car_value, $tax_percentage, $instalments): string
+    {
+        $result = "";
         try {
-            $this->calculatePremium($basePremium, $instalments, $tax_percentage);
+            $base_premium = $estimated_car_value * ($this->rate / 100); // Calculating by the premium percentage
+            $commission = $base_premium * ($this->commissionRate / 100);
+            $total_tax = $base_premium * ($tax_percentage / 100);
+            $total_cost = ($base_premium + $commission + $total_tax);
+            $result = $this->createHtml($base_premium, $commission, $total_tax, $total_cost, $instalments, $estimated_car_value, $tax_percentage);
+        } catch (\Exception $ex) {
+            echo $ex->getMessage();
+        }
+        return $result;
+    }
 
+
+    /**
+     * @param float $base_premium
+     * @param float $commission
+     * @param float $total_tax
+     * @param float $total_cost
+     * @param float $instalments
+     * @param float $estimated_car_value
+     * @param float $tax_percentage
+     * @return string
+     *Creating the html template with the dynamic variables
+     */
+    private function createHtml(float $base_premium, float $commission, float $total_tax, float $total_cost, float $instalments, float $estimated_car_value, float $tax_percentage): string
+    {
+        $html = '';
+        try {
+            $html = '<table style="width:100%"> <tr> <th></th><th>Policy</th>';
+            for ($i = 0; $i < $instalments; $i++) {
+                $html .= "<th>Installment #" . ($i + 1) . "</th>";
+            }
+            $html .= "  </tr> <tr>";
+            $html .= "<td>Value </td ><td>" . $this->roundOff($estimated_car_value) . " </td > ";
+            for ($i = 0; $i < $instalments; $i++) {
+                $html .= "<td></td>";
+            }
+            $html .= "</tr>";
+
+            $html .= "<td>Base Premium (" . ($this->rate) . " %)  </td ><td>" . $this->roundOff($base_premium) . " </td > ";
+            for ($i = 0; $i < $instalments; $i++) {
+                $html .= "<td>" . ($this->roundOff($base_premium / $instalments)) . "</td>";
+            }
+            $html .= "</tr>";
+
+            $html .= "<td>Commission (17 %)  </td ><td>" . $this->roundOff($commission) . " </td > ";
+            for ($i = 0; $i < $instalments; $i++) {
+                $html .= "<td>" . ($this->roundOff($commission / $instalments)) . "</td>";
+            }
+            $html .= "</tr>";
+
+            $html .= "<td>Tax (" . ($tax_percentage) . ") % </td ><td>" . $this->roundOff($total_tax) . " </td > ";
+            for ($i = 0; $i < $instalments; $i++) {
+                $html .= "<td>" . ($this->roundOff($total_tax / $instalments)) . "</td>";
+            }
+            $html .= "</tr>";
+
+            $html .= "<td><b>Total Cost</b> </td ><td><b>" . $this->roundOff($total_cost) . "</b> </td > ";
+            for ($i = 0; $i < $instalments; $i++) {
+                $html .= "<td>" . ($this->roundOff($total_cost / $instalments)) . "</td>";
+            }
+            $html .= "</tr>";
+            $html .= "</table > ";
         } catch (\Exception $ex) {
             echo $ex->getMessage();
         }
 
+        return $html;
     }
 
 
     /**
-     * @param $basePremium
-     * @param $estimated_car_value
-     * @param $tax_percentage
-     *
-     * Method doing the calculations of the required data
+     * @param $number
+     * @return string
+     * Rounding off the number to precision
      */
-    public function calculatePremium($basePremium, $estimated_car_value, $tax_percentage)
+    public function roundOff($number)
     {
-
+        $result = number_format((float)$number, 2, '.', '');
+        return $result;
     }
-
 
 }
 
 
-?>
